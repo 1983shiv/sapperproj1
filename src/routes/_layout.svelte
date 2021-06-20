29ec7834b4c6f28f2742f12867e5dd64c6b03b85
1983shiv <1983.shiv@gmail.com>
2021-06-20 12:33:18 +0530

    Updates

--- src/routes/_layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import Nav2 from "../components/Nav2.svelte";
-  import Footer from "../components/Footer.svelte";
-  export let segment;
-</script>
-
-<Nav2 {segment} />
-<slot />
-<Footer />
